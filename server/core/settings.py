ec940b5beb99273b2d79125e4d809e5318c3e335
Nameless <nameies@yandex.ru>
2023-07-04 14:59:39 +0300

    Now client loads variables from file.

--- server/core/settings.py
+++ server/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b3u4nbph(^j(r%(4w-9_fake0z)nbnj77!jaz4!j$i@=dqeusy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
